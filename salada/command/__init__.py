be8fcad91836d07bb7ad697c004aa9317269ae77
Tomoya Kose <tomoya@mitsuse.jp>
2016-02-23 21:19:20 +0900

    Define subcommands.

--- salada/command/__init__.py
+++ salada/command/__init__.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 
+
+@click.group()
 def main():
     pass
+
+
+@main.command()
+def gather():
+    '''Extract comments from the given source files.'''
+
+
+@main.command()
+def prepare():
+    '''Segment strings into tokens'''
+
+
+@main.command()
+def build():
+    '''Construct a prediction model and save it.'''
+
+
+@main.command()
+def serve():
+    '''Launch a K-best word completion server.'''
+
+
+@main.command()
+def request():
+    '''Predict next words with the given prefix.'''
 
 
 if __name__ == '__main__':
