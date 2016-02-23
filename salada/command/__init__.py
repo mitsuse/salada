316c259711aec0b2c3aa89a689a9429c5dfb1d6e
Tomoya Kose <tomoya@mitsuse.jp>
2016-02-23 23:12:29 +0900

    `salada serve` launches a completion server with the given port.

--- salada/command/__init__.py
+++ salada/command/__init__.py
@@ -25,8 +25,25 @@ def build():
 
 
 @main.command()
-def serve():
+@click.option(
+    '--port', type=int,
+    default=9000, show_default=True,
+    help='The port number to be used by the server.',
+)
+def serve(port):
     '''Launch a K-best word completion server.'''
+
+    from gevent.server import StreamServer
+    from salada import rpc
+
+    # TODO: Specify the segmenter and the completer by command-line arguments.
+    service = rpc.Service(
+        segmenter=None,
+        completer=None,
+    )
+
+    server = StreamServer(('localhost', port), service)
+    server.serve_forever()
 
 
 @main.command()
