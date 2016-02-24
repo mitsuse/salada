17b98b40a75cd6e265762f4e00b3e9f00683793c
Tomoya Kose <tomoya@mitsuse.jp>
2016-02-24 23:47:39 +0900

    Group test cases by class for readability.

--- tests/salada/test_language.py
+++ tests/salada/test_language.py
@@ -4,31 +4,28 @@
 from salada import language
 
 
-def test_segument_equality():
-    a = language.Segment('foo', True, False)
-    b = language.Segment('foo', True, False)
-    assert a == b and b == a
+class TestSegment:
+    def equals_to_other_which_has_same_proporties(self):
+        a = language.Segment('foo', True, False)
+        b = language.Segment('foo', True, False)
+        assert a == b and b == a
 
+    def test_doesnt_equal_to_other_which_has_different_surface(self):
+        a = language.Segment('foo', True, False)
+        b = language.Segment('bar', True, False)
+        assert a != b and b != a
 
-def test_segument_equality_different_surface():
-    a = language.Segment('foo', True, False)
-    b = language.Segment('bar', True, False)
-    assert a != b and b != a
+    def test_doesnt_equal_to_other_which_is_headless_unexpectedly(self):
+        a = language.Segment('foo', False, False)
+        b = language.Segment('foo', True, False)
+        assert a != b and b != a
 
+    def test_doesnt_equal_to_other_which_is_tailless_unexpectedly(self):
+        a = language.Segment('foo', True, False)
+        b = language.Segment('foo', True, True)
+        assert a != b and b != a
 
-def test_segument_equality_different_head_status():
-    a = language.Segment('foo', False, False)
-    b = language.Segment('foo', True, False)
-    assert a != b and b != a
-
-
-def test_segument_equality_different_tail_status():
-    a = language.Segment('foo', True, False)
-    b = language.Segment('foo', True, True)
-    assert a != b and b != a
-
-
-def test_segument_equality_different_type():
-    a = language.Segment('foo', True, False)
-    b = 1
-    assert a != b and b != a
+    def test_doesnt_equal_to_a_value_of_other_type(self):
+        a = language.Segment('foo', True, False)
+        b = 1
+        assert a != b and b != a
--- tests/salada/test_segmenter.py
+++ tests/salada/test_segmenter.py
@@ -5,38 +5,37 @@ from salada import language
 from salada import segmenter
 
 
-def test_default_segment():
-    text = '    foo \n \n\n   bar \t\n baz  '
-    expectation = [
-        language.Segment('', True, False),
-        language.Segment('foo', False, False),
-        language.Segment('bar', False, False),
-        language.Segment('baz', False, False),
-        language.Segment('', False, True),
-    ]
-    result = segmenter.Default().segment(text)
-    assert result == expectation
+class TestDefault:
+    def test_segment_text_by_sequence_of_spaces(self):
+        text = '    foo \n \n\n   bar \t\n baz  '
+        expectation = [
+            language.Segment('', True, False),
+            language.Segment('foo', False, False),
+            language.Segment('bar', False, False),
+            language.Segment('baz', False, False),
+            language.Segment('', False, True),
+        ]
+        result = segmenter.Default().segment(text)
+        assert result == expectation
 
+    def test_regard_first_as_headless(self):
+        text = 'foo \n \n\n   bar \t\n baz  '
+        expectation = [
+            language.Segment('foo', True, False),
+            language.Segment('bar', False, False),
+            language.Segment('baz', False, False),
+            language.Segment('', False, True),
+        ]
+        result = segmenter.Default().segment(text)
+        assert result == expectation
 
-def test_default_segment_first_headless():
-    text = 'foo \n \n\n   bar \t\n baz  '
-    expectation = [
-        language.Segment('foo', True, False),
-        language.Segment('bar', False, False),
-        language.Segment('baz', False, False),
-        language.Segment('', False, True),
-    ]
-    result = segmenter.Default().segment(text)
-    assert result == expectation
-
-
-def test_default_segment_last_tailless():
-    text = '    foo \n \n\n   bar \t\n baz'
-    expectation = [
-        language.Segment('', True, False),
-        language.Segment('foo', False, False),
-        language.Segment('bar', False, False),
-        language.Segment('baz', False, True),
-    ]
-    result = segmenter.Default().segment(text)
-    assert result == expectation
+    def test_regard_last_as_tailless(self):
+        text = '    foo \n \n\n   bar \t\n baz'
+        expectation = [
+            language.Segment('', True, False),
+            language.Segment('foo', False, False),
+            language.Segment('bar', False, False),
+            language.Segment('baz', False, True),
+        ]
+        result = segmenter.Default().segment(text)
+        assert result == expectation
